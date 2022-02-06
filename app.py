7388f5baf96d76affea616b8a071d279b02fa80c
Jacques Pierre <pierreandassoc.gmail>
2022-02-06 15:30:33 -0700

    feat: if the user tries to tinker with the URL to visit questions out of order, a message telling       them they're trying to access an invalid question will flash as part of the redirect

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from crypt import methods
-from flask import Flask, request, render_template, redirect, flash, jsonify
+from flask import Flask, request, render_template, redirect, flash
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 
@@ -8,6 +8,8 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
+
+INVALID_QUESTION_MSG = 'You attempted to access an invalid question.'
 
 responses = []
 
@@ -23,6 +25,7 @@ def handle_question(question):
 	if len(responses) < len(satisfaction_survey.questions):
 		if int(question) == len(responses):
 			return render_template('show_question_page.html', question=int(question), satisfaction=satisfaction_survey)
+		flash(INVALID_QUESTION_MSG)
 		return redirect(f'/questions/{len(responses)}')
 	return render_template('/thankyou.html', satisfaction=satisfaction_survey)
 
